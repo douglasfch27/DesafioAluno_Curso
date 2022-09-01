737881807f9d4260f4c67e9fdae58260e1b3cd9c
Douglas Charão <55717330+douglasfch27@users.noreply.github.com>
2022-09-01 10:59:05 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DesafioZamba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Cadastro = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ListaOrdenada = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME ALUNO:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CURSO:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(392, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(392, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LISTAR POR CURSO";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Cadastro
+            // 
+            this.Cadastro.Location = new System.Drawing.Point(15, 150);
+            this.Cadastro.Name = "Cadastro";
+            this.Cadastro.Size = new System.Drawing.Size(102, 33);
+            this.Cadastro.TabIndex = 5;
+            this.Cadastro.Text = "CADASTRAR";
+            this.Cadastro.UseVisualStyleBackColor = true;
+            this.Cadastro.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "LISTAR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ListaOrdenada
+            // 
+            this.ListaOrdenada.FormattingEnabled = true;
+            this.ListaOrdenada.Location = new System.Drawing.Point(277, 125);
+            this.ListaOrdenada.Name = "ListaOrdenada";
+            this.ListaOrdenada.Size = new System.Drawing.Size(215, 264);
+            this.ListaOrdenada.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListaOrdenada);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Cadastro);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Desafio1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Cadastro;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox ListaOrdenada;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioZamba1
+{
+    internal static class Desafio1
+    {
+        /// <summary>
+        /// Programa para cadastrar aluno e curso e exibir lista ordenada
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+        public static void bolha(List<Aluno> lista)
+        {
+            int i;
+
+
+            for (i = 0; i < lista.Count - 1; i++)
+            {
+                posMenor = i;
+                for (j = i + 1; j < lista.Count; j++)
+                {
+                    if (lista[j] < lista[posMenor])
+                    {
+                        posMenor = j;
+                    }
+                }
+
+                if (i != posMenor)
+                {
+                    tmp = lista[i];
+                    lista[i] = lista[posMenor];
+                    lista[posMenor] = tmp;
+                }
+            }
+        }
+    }
+}
